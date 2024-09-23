32a5946844137a4da7d36b32e2c5908dc0d2bd61
sifat7890 <sifath740@gmail.com>
2024-09-24 01:38:07 +0600

    simple login with html

--- js/login.js
+++ js/login.js
@@ -2,10 +2,22 @@
 document.getElementById('button-login').addEventListener('click', function (event) {
     // step 2: prevent default behavior (prevent reload browser)
     event.preventDefault();
-    console.log('login button clicked');
+
 
     // step 3:get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-    
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    // step 4: validate phone and pin
+    if (phoneNumber === '0123456789' && pinNumber === '1234') {
+        console.log('You are locked in');
+        window.location.href = '/home.html';
+
+    }
+    else {
+        alert('Wrong phone number or Pin')
+    }
+
 })
+
